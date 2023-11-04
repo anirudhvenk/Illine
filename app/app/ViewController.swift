c08f486dc2d58d78ed8a4287d13bb3aa50394e13
Anirudh Venkatraman <anirudhv05ca@gmail.com>
2023-11-03 21:05:28 -0500

    added multipeer

--- app/app/ViewController.swift
+++ app/app/ViewController.swift
@@ -6,11 +6,49 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MCSessionDelegate, MCBrowserViewControllerDelegate {
+    var peerID: MCPeerID!
+    var mcSession: MCSession!
+    var mcAdvertiserAssistant: MCAdvertiserAssistant!
 
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+            case .connected: print ("connected \(peerID)")
+            case .connecting: print ("connecting \(peerID)")
+            case .notConnected: print ("not connected \(peerID)")
+            default: print("unknown status for \(peerID)")
+        }
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+    }
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        peerID = MCPeerID(displayName: UIDevice.current.name)
+        mcSession = MCSession(peer: peerID, securityIdentity: nil,  encryptionPreference:.required)
+        mcSession.delegate = self
         // Do any additional setup after loading the view.
     }
 
